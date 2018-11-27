aa5363fa3952774231cc92ffa74403a07628f000
Goblues <37900419+Goblues@users.noreply.github.com>
2018-11-27 21:49:48 +0900

    algorithm

---
+++ BOJ SOLVE/5217 쌍의 합.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a;
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a);
+		for (int j = 1; j < a; j++)
+		{
+			for (int k = 1; k < a; k++)
+			{
+				if (j != k)
+				{
+					if ((j + k) == a)
+				}
+				else
+				{
+					break;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
